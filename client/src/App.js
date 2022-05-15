0534d39905438056f98f8aec95e73635d90fc44f
Brian Shimkus <brianshimkus1@gmail.com>
2022-05-15 14:10:23 -0500

    Built out Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import Landing from './pages/landing'
+
 function App() {
 	return (
 		<div className='App'>
 			<h1>Jobify</h1>
+			<Landing />
 		</div>
 	)
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import 'normalize.css'
 import './index.css'
 import App from './App'
@@ -9,6 +10,8 @@ const root = createRoot(container)
 
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
