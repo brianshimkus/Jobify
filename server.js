dc6e50987718b4a9bcf3e9b41d01f3c1aac41699
Brian Shimkus <brianshimkus1@gmail.com>
2022-05-17 23:08:16 -0500

    Got basic server up and running

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Welcome!')
+})
+
+const port = process.env.PORT || 6000
+
+app.listen(port, () => console.log(`Server is running on port ${port}...`))
