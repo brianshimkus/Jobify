c61eed96c0096e31a94cf8c746f2e2cafee45895
Brian Shimkus <brianshimkus1@gmail.com>
2022-06-15 22:43:12 -0500

    Set up BadRequestError

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,12 @@ import { StatusCodes } from 'http-status-codes'
 class CustomAPIError extends Error {
 	constructor(message) {
 		super(message)
+	}
+}
+
+class BadRequestError extends CustomAPIError {
+	constructor(message) {
+		super(message)
 		this.statusCode = StatusCodes.BAD_REQUEST
 	}
 }
@@ -12,7 +18,7 @@ const register = async (req, res) => {
 	const { name, email, password } = req.body
 
 	if (!name || !email || !password) {
-		throw new CustomAPIError('please provide all values')
+		throw new BadRequestError('please provide all values')
 	}
 
 	const user = await User.create({ name, email, password })
