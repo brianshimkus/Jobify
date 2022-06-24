65c14a887d4e11aff8219a5454bb15f3f5de7321
Brian Shimkus <brianshimkus1@gmail.com>
2022-06-23 23:58:54 -0500

    Fetch error

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,3 +1,18 @@
+import { useEffect } from 'react'
+
 export default function Dashboard() {
+	const fetchData = async () => {
+		try {
+			const response = await fetch('http://localhost:4000/')
+			const data = await response.json()
+			console.log(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
 	return <div>Dashboard</div>
 }
