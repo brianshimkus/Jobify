2960295e7dcab5fa3edfe83c46f9c6a66d45c24f
Brian Shimkus <brianshimkus1@gmail.com>
2022-07-10 23:43:51 -0500

    Set up Morgan package

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ import connectDB from './db/connect.js'
 import authRouter from './routes/authRoutes.js'
 import jobsRouter from './routes/jobsRoutes.js'
 import 'express-async-errors'
+import morgan from 'morgan'
 
 const app = express()
 dotenv.config()
+
+if (process.env.NODE_ENV !== 'production') {
+	app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
